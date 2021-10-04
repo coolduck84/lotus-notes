0a881166da80456b7b57aafffa206daf7df6c64d
coolduck84 <58141427+coolduck84@users.noreply.github.com>
2021-10-04 15:37:37 +0530

    Initial Commit

---
+++ src/test/java/com/lotus/notes/lotusnotes/LotusNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotus.notes.lotusnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotusNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
